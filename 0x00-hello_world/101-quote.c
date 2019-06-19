810f9ea206066b1b0bb577516c76067aeb698b0b
Dario <dario.castano@aim.com>
2019-06-19 18:45:52 +0000

    Add comment to 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
 #include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - \
