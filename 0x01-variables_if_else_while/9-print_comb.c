5e026547b743a5d64c76f5c86f393ada11b415f1
Dario <dario.castano@aim.com>
2019-06-20 19:37:09 +0000

    Add 9-print_comb.c to 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
