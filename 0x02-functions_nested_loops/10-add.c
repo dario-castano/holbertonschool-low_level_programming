89ecfcc802683ad000c4526cfd3491ac11fc9c86
Dario <dario.castano@aim.com>
2019-06-25 16:03:43 +0000

    Add 10-add.c answer to 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - get the sum of a pair of integers.
+ * @a: A integer number
+ * @b: A integer number
+ *
+ * Return: the sum between a and b (a+b)
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
