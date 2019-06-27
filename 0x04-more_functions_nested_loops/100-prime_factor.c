1f6c87026ce94630d932c66107f16ee0e5b9b6f7
Dario <dario.castano@aim.com>
2019-06-27 21:55:18 +0000

    Add 100-prime_factor.c to 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+/**
+ * largest_prime_factor - check the code for Holberton School students.
+ * @a: A number
+ *
+ * Return: Always 0.
+ */
+unsigned long int largest_prime_factor(unsigned long int a)
+{
+	unsigned long int b = 2;
+
+	while (a > b)
+	{
+		if (a % b == 0)
+		{
+			a = a / b;
+			b = 2;
+		}
+		else
+			b++;
+	}
+	return (b);
+} 
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%lu\n", largest_prime_factor(612852475143));
+	return (0);
+}
