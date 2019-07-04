c14c3ab44488e9c76a07856fec9dbc93548f0c0f
Dario <dario.castano@aim.com>
2019-07-04 23:27:31 +0000

    Fix condition on 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,12 +14,12 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	int j = 0;
 
-	for (i = n - 1; i > 0; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		buf[j] = a[i];
 		j++;
 	}
-	for (i = 0; i < n - 1 ; i++)
+	for (i = 0; i < n; i++)
 	{
 		a[i] = buf[i];
 	}
