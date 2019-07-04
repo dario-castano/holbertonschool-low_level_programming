0ae89da9727b61e6f8cb111a6bc2ffa2d9fb99a3
Dario <dario.castano@aim.com>
2019-07-04 23:36:48 +0000

    Fix returns on 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,9 +19,9 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (15);
+			return (s1[i] - s2[i]);
 		else if (s1[i] < s2[i])
-			return (-15);
+			return (-1 * (s2[i] - s1[i]));
 		i++;
 	}
 	return (res);
