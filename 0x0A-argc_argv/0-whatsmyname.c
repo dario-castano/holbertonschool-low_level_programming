47f1c73149569b2cc5ca6976ed10bf6c7ac7c41b
Dario <dario.castano@aim.com>
2019-07-12 17:03:32 +0000

    Add 0-whatsmyname.c to 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: array of params
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		puts(argv[0]);
+	}
+
+	return (0);
+}
