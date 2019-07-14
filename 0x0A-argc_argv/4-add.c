069a8dadc56f50649b7d420ac90ed8f36cb09839
Dario <dario.castano@aim.com>
2019-07-14 18:53:22 +0000

    Add int overflow check to 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
- * main - Prints the sum of n arguments
- * @argc: args
- * @argv: array of args
- * Return: sum if success, 1 if error
-**/
+* isa_positive_number - check if a string is a positve number
+* @str: array of chars
+* Return: 1 if number, 0 if not
+*/
+int isa_positive_number(char *str)
+{
+	if (*str)
+	{
+		if (*str < 48 || *str > 57)
+			return (0);
+		else
+			return (isa_positive_number(str + 1));
+	}
+	return (1);
+}
 
+/**
+* has_only_numbers - shows if an array contain only numbers
+* @arr: An array of chars
+* Return: 1 if has only numbers,
+* otherwise 0
+*/
+int has_only_numbers(char **arr)
+{
+	if (*arr != 0)
+	{
+		if (!isa_positive_number(*arr))
+			return (0);
+		else
+			return (has_only_numbers(arr + 1));
+	}
+	return (1);
+}
+
+/**
+* main - entry point
+* @argc: param count
+* @argv: array of args
+* Return: 1 if Error, otherwise 0
+*/
 int main(int argc, char **argv)
 {
+	int i = 1;
+	int acc = 0;
+	int check = 0;
 
-int i;
-unsigned int acc = 0;
-
-for (i = 1; i < argc; i++)
-{
-	if (*argv[i] >= 48 && *argv[i] <= 57)
-		acc += (unsigned int)atoi(argv[i]);
+	if (argc < 2)
+	{
+		puts("0");
+		return (0);
+	}
 	else
 	{
-		printf("Error\n");
-		return (1);
-	}
-}
-printf("%u\n", acc);
+		if (has_only_numbers(argv + 1))
+		{
+			while (argv[i] != 0)
+			{
+				acc += atoi(argv[i]);
 
-return (acc);
+				if (acc < check)
+				{
+					puts("Error");
+					return (1);
+				}
+
+				check += atoi(argv[i]);
+				i++;
+			}
+			printf("%d\n", acc);
+			return (acc);
+		}
+		else
+		{
+			puts("Error");
+			return (1);
+		}
+	}
 }
