d38719d38e82d20cd69ac769556b784ec405cb3f
Dario <dario.castano@aim.com>
2019-07-22 14:45:24 +0000

    Initial commit for 0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define FILENAME __FILE__
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", FILENAME);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+# 0x0D-preprocessor
+This is the 0x0D-preprocessor repo for Holberton School
