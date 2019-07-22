8c3713a0ddfe9bb700cb5d00063b08a3a86a6560
Dario <dario.castano@aim.com>
2019-07-22 18:59:43 +0000

    Fix Age on 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * print_dog - function that prints a struct dog
+ * @d: dog struct
+ * Return: void
+ */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
@@ -11,10 +16,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (d->age <= 0.0)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
