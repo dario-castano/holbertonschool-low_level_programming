f3e90b78c1a624a493e02d9bfe6358439f2e9f4b
Dario <dario.castano@aim.com>
2019-08-01 22:46:14 +0000

    Fix betty on 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -43,7 +43,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
 
 	new_node->len = _strlen(new_node->str);
