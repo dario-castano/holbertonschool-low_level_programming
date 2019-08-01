2427fb5b721cac71d431d88525c2156c0da743fd
Dario <dario.castano@aim.com>
2019-08-01 22:11:32 +0000

    Add 100-first.c to the repo

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * hare_tortoise - function that runs before main
+ * Return: void
+ */
+void hare_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
