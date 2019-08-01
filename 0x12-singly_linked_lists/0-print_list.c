6c9f82f68b738ce251de86ee84b38a315470f996
Dario <dario.castano@aim.com>
2019-08-01 20:46:14 +0000

    Fix broken file on 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,15 @@
 size_t print_list(const list_t *h)
 {
 	int num_nodes;
-	const list_t *pointer;
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
-		pointer = pointer->next;
+		printf("[%d] %s\n",
+		 h->str ?
+		 h->len : 0,
+		 h->str ?
+		 h->str : "(nil)");
+		h = h->next;
 		num_nodes++;
 	}
 
