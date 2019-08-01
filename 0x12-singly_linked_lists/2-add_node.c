8de536592af166cd0aea9037126f2dd1d42287fd
Dario <dario.castano@aim.com>
2019-08-01 20:58:02 +0000

    Add 2-add_node.c to the repo

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,10 +3,27 @@
 #include "lists.h"
 
 /**
+ * _strlen - swap the values of two integers
+ * @s: pointer to a char
+ * Return: number of chars
+ */
+int _strlen(char *s)
+{
+	int acc = 0;
+
+	while (s[acc] != '\0')
+	{
+		acc++;
+	}
+
+	return (acc);
+}
+
+/**
  * add_node -adds a new node at the beginning of a list_t list.
  * @head: the head node
  * @str: the string to add
- *
+ * Return: list_t node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -19,9 +36,11 @@ list_t *add_node(list_t **head, const char *str)
 		if (new_node == NULL)
 			return (NULL);
 
-		new_node->str = str;
-		new_node->len = head->len;
+		new_node->str = strdup(str);
+		new_node->len = _strlen(new_node->str);
 		new_node->next = *head;
+
+		*head = new_node;
 
 		return (new_node);
 	}
