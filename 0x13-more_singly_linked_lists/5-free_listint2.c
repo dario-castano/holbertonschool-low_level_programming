81498adccae796b84229665bc022fa31127753a2
Dario <dario.castano@aim.com>
2019-08-06 22:22:45 +0000

    Remove redundant if on 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head)
+	while (*head)
 	{
-		while (*head)
-		{
-			ptr = *head;
-			*head = ptr->next;
-			free(ptr);
-		}
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
 	}
 
 	*head = NULL;
