a92760b15d4235fb7269635aa095ebe843f91151
Dario <dario.castano@aim.com>
2019-08-08 22:47:21 +0000

    Add 2-get_bit.c to the repo

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer
+ * @index: index of the binary
+ * Return: the value of the bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	n >>= index;
+
+	if (((n >> 1) << 1) == n)
+		return (0);
+	else
+		return (1);
+}
