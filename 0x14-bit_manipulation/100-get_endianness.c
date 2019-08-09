77ec952affb152cb812999b60402946eebf60f93
Dario <dario.castano@aim.com>
2019-08-09 00:16:45 +0000

    Add 100-get_endianness.c to the repo

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * get_endianness -  checks the endianness
+ * Return: void
+ *
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *ch = (char *) &n;
+	if (*ch)
+		return (1);
+	return (0);
+}
