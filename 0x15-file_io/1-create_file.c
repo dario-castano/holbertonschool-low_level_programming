b632c3ac570443f1ca6aa986729deb26e2d3f58f
Dario <dario.castano@aim.com>
2019-08-13 21:49:09 +0000

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -55,8 +55,11 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (w_bytes == IO_ERROR)
+	{
+		close(fd);
 		return (-1);
-	else
-		return (1);
+	}
 
+	close(fd);
+	return (1);
 }
