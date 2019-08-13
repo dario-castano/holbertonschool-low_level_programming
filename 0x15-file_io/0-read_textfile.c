a6a206ff4ae88142569dca7ca73dd29a7ebb6e80
Dario <dario.castano@aim.com>
2019-08-13 19:37:48 +0000

    Initial commit for 0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,63 @@
+#include "holberton.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define _ERROR -1
+
+/**
+ * read_textfile -  reads a text file and prints it to
+ * the POSIX standard output.
+ *
+ * @filename: Path and name of the file to be read
+ * @letters: Number of letters to be read
+ *
+ * Return: Number the actual number of
+ * letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fd, r_bytes, w_bytes;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == _ERROR)
+		return (0);
+
+	buffer = malloc(letters + 1);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	buffer[letters] = '\0';
+	r_bytes = read(fd, buffer, letters);
+
+	if (r_bytes == _ERROR)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	w_bytes = write(STDOUT_FILENO, buffer, letters);
+
+	if (w_bytes == _ERROR)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (w_bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+# 0x15-file_io
+This is the 0x15-file_io repo for Holberton School
