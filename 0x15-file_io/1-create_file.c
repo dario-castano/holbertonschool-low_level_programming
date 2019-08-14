f092e531d592d9ade3c7d7d8b29beceef1e3efcd
Dario <dario.castano@aim.com>
2019-08-14 00:32:50 +0000

    Add octal mode on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == IO_ERROR)
 		return (-1);
