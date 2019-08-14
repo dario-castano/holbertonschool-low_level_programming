578e83b1b076c8a41d2a5e830ac7e0888ace22c9
Dario <dario.castano@aim.com>
2019-08-14 00:29:28 +0000

    Fix output on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == IO_ERROR)
 		return (-1);
@@ -54,7 +54,7 @@ int create_file(const char *filename, char *text_content)
 		w_bytes = write(fd, text_content, text_size);
 	}
 
-	if (w_bytes == IO_ERROR)
+	if (w_bytes == IO_ERROR || w_bytes != text_size)
 	{
 		close(fd);
 		return (-1);
