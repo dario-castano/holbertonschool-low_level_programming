69336179a2a65110819292d43b793aa6c1aa288b
Dario Castaño <dario.castano@aim.com>
2019-09-14 17:27:58 -0500

    Avoid add node on empty list on 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,8 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr, *new_node, *prev_node;
 	unsigned int i = 0;
 
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -29,8 +31,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
-			if (idx == 0)
-				return (add_dnodeint(h, n));
 			if (ptr->next == NULL)
 				return (add_dnodeint_end(h, n));
 			prev_node->next = new_node;
