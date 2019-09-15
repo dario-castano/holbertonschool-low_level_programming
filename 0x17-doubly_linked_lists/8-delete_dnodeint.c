2ffac9dfd0c7df3342e904244c23f186e8fa7d44
Dario Castaño <dario.castano@aim.com>
2019-09-14 19:54:00 -0500

    Add  8-delete_dnodeint.c to the repo

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node, *prev_node, *next_node;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	
+	node = *head;
+	prev_node = node->prev;
+	next_node = node->next;
+
+	if (index == 0)
+	{
+		if (next_node == NULL)
+			*head = NULL;
+		else
+		{
+			*head = next_node;
+			next_node->prev = NULL;
+		}
+		free(node);
+		return (1);
+	}
+
+	while (node != NULL)
+	{
+		if (i == index)
+		{
+			prev_node->next = next_node;
+			next_node->prev = prev_node;
+			free(node);
+			return (1);
+		}
+		node = node->next;
+		prev_node = node->prev;
+		next_node = node->next; 
+		i++;
+	}
+	return (-1);
+}
