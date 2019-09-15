b860a97c63d3bd87dd0d20d99a19d9ad2c06d88d
Dario Castaño <dario.castano@aim.com>
2019-09-15 11:00:54 -0500

    Fix bad insertion on 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,23 +16,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
+
 	ptr = *h;
 	prev_node = NULL;
+
 	while (ptr != NULL)
 	{
 		if (i == idx)
 		{
-			if (ptr->next == NULL)
-			{
-				free(new_node);
-				return (add_dnodeint_end(h, n));
-			}
 			prev_node->next = new_node;
 			new_node->prev = prev_node;
 			new_node->next = ptr;
@@ -43,6 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 		i++;
 	}
+	
 	free(new_node);
 	return (NULL);
 }
