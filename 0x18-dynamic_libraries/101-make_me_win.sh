44d9271091886a0a70bfeed7b735dcd11e278749
Dario Castaño <dario.castano@aim.com>
2019-09-16 21:10:46 +0000

    Remove -O in 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-wget -O ../libinject.so https://github.com/dario-castano/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libinject.so
+wget -P ../ https://github.com/dario-castano/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libinject.so
 export LD_PRELOAD=../libinject.so
