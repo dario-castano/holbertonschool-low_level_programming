b1f83b4cd81518e95e671f1b0aa5c136373a0385
Dario Castaño <dario.castano@aim.com>
2019-10-29 10:36:01 -0500

    Fix merge and and add dealer

--- 0x1B-sorting_algorithms/103-merge_sort.c
+++ 0x1B-sorting_algorithms/103-merge_sort.c
@@ -1,58 +1,79 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
- * swap - swaps two array pointers
- * @left: A pointer
- * @right: Another pointer
- * Return: void
- */
-void swap(int *left, int *right)
-{
-	int temp;
-
-	temp = *left;
-	*left = *right;
-	*right = temp;
-}
-
-/**
- * 
- * 
- * 
- * 
- * 
- */
-void merge(int *array, int left, int middle, int right)
-{
-
-}
-
-/**
- * sort - recursive part of the merge sort array
- * 
- * 
- * 
- */
-void sort(int *array, int left, int right)
-{
-    int middle = left + ((right - left) / 2);
-
-    sort(array, left, middle);
-    sort(array, middle, right);
-    merge(array, left, middle, right);
-}
-
-/**
- * merge_sort -  sorts an array of integers in ascending order
- * using the Merge sort algorithm
- * @array: Pointer to the array
+ * merge - merge function of the mergesort
+ * @array: pointer to array
  * @size: size of the array
+ * @left: pointer to left array
+ * @right: pointer to right array
  * Return: void
- */
+ **/
+void merge(int *array, int *left, int *right, size_t size)
+{
+	int i, j, k, lsize, rsize;
+
+	lsize = size / 2;
+	rsize = size - lsize;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(left, lsize);
+	printf("[right]: ");
+	print_array(right, rsize);
+
+    i = 0;
+    j = 0;
+    k = 0;
+
+	while (i < lsize && j < rsize)
+	{
+		if (left[i] < right[j])
+			array[k++] = left[i++];
+		else
+			array[k++] = right[j++];
+	}
+
+	while (i < lsize)
+		array[k++] = left[i++];
+
+	while (j < rsize)
+		array[k++] = right[j++];
+
+	printf("[Done]: ");
+	print_array(array, size);
+}
+
+/**
+ * merge_sort - sorts an array with merge sort
+ * @array: array to be evaluated
+ * @size: array size
+ * Return: void
+ **/
 void merge_sort(int *array, size_t size)
 {
-    if (size < 2)
-        return;
-    
-    sort(array, 0, size);
+	size_t middle;
+    size_t i;
+    int *pack;
+	int *left;
+	int *right;
+
+	if (array == NULL || size < 2)
+		return;
+
+	middle = size / 2;
+    pack = malloc(sizeof(int) * size);
+    left = pack;
+    right = &pack[middle];
+
+	for (i = 0; i < middle; i++)
+		left[i] = array[i];
+
+	for (i = middle; i < size; i++)
+		right[i - middle] = array[i];
+
+	merge_sort(left, middle);
+	merge_sort(right, size - middle);
+	merge(array, left, right, size);
+    free(pack);
 }
