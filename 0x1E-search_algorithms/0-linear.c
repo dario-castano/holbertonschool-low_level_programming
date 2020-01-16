0b903199fc95f08c2b628341ccf84ede75447232
Dario Castaño <dario.castano@aim.com>
2020-01-16 09:07:31 -0500

    Add task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,26 @@
 #include "search_algos.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * linear_search - linear searching algorithm
+ * @array: Array to search
+ * @size: Size of the array
+ * @value: Value to search
+ * Return: int
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
